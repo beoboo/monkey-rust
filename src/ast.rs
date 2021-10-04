e5b9aa1dc09aa1777572f4421f19485cef884032
Pietro Atzeni <pietrino.atzeni@moneyfarm.com>
2021-10-04 23:12:02 +0200

    Added prefix and infix expressions

--- src/ast.rs
+++ src/ast.rs
@@ -1,16 +1,17 @@
-use crate::token::Token;
+use core::fmt;
 use std::any::Any;
+use std::fmt::{Display, Formatter};
+
+use crate::token::Token;
 
 pub trait Node {
     fn token_literal(&self) -> &str;
     fn as_any(&self) -> &dyn Any;
 }
 
-pub trait Statement: Node {
-}
+pub trait Statement: Node + fmt::Display {}
 
-pub trait Expression: Node {
-}
+pub trait Expression: Node + fmt::Display {}
 
 pub struct Program {
     pub statements: Vec<Box<dyn Statement>>,
@@ -30,42 +31,17 @@ impl Node for Program {
     }
 }
 
-pub struct Identifier {
-    pub token: Token,
-    pub value: String,
-}
+impl Display for Program {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        for stmt in &self.statements {
+            write!(f, "{}", stmt)?;
+        }
 
-impl Node for Identifier {
-    fn token_literal(&self) -> &str {
-        return self.token.literal.as_str();
-    }
-
-    fn as_any(&self) -> &dyn Any {
-        self
+        Ok(())
     }
 }
 
-impl Expression for Identifier {
-}
-
-pub struct DummyExpression {
-
-}
-
-impl Node for DummyExpression {
-    fn token_literal(&self) -> &str {
-        "dummy"
-    }
-
-    fn as_any(&self) -> &dyn Any {
-        self
-    }
-}
-
-impl Expression for DummyExpression {
-
-}
-
+// Statements
 pub struct LetStatement {
     pub token: Token,
     pub name: Identifier,
@@ -82,7 +58,13 @@ impl Node for LetStatement {
     }
 }
 
-impl Statement for LetStatement {
+impl Statement for LetStatement {}
+
+
+impl Display for LetStatement {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} {} = {};", self.token_literal(), self.name, self.value)
+    }
 }
 
 pub struct ReturnStatement {
@@ -100,5 +82,179 @@ impl Node for ReturnStatement {
     }
 }
 
-impl Statement for ReturnStatement {
+impl Statement for ReturnStatement {}
+
+impl Display for ReturnStatement {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} {};", self.token_literal(), self.return_value)
+    }
+}
+
+
+pub struct ExpressionStatement {
+    pub token: Token,
+    pub expression: Box<dyn Expression>,
+}
+
+impl Node for ExpressionStatement {
+    fn token_literal(&self) -> &str {
+        return self.token.literal.as_str();
+    }
+
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+}
+
+impl Statement for ExpressionStatement {}
+
+impl Display for ExpressionStatement {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.expression)
+    }
+}
+
+// Expressions
+
+pub struct DummyExpression {}
+
+impl Node for DummyExpression {
+    fn token_literal(&self) -> &str {
+        "dummy"
+    }
+
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+}
+
+impl Expression for DummyExpression {}
+
+impl Display for DummyExpression {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "dummy")
+    }
+}
+
+pub struct Identifier {
+    pub token: Token,
+    pub value: String,
+}
+
+impl Node for Identifier {
+    fn token_literal(&self) -> &str {
+        return self.token.literal.as_str();
+    }
+
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+}
+
+impl Expression for Identifier {}
+
+impl Display for Identifier {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.value)
+    }
+}
+
+pub struct IntegerLiteral {
+    pub token: Token,
+    pub value: i64,
+}
+
+impl Node for IntegerLiteral {
+    fn token_literal(&self) -> &str {
+        return self.token.literal.as_str();
+    }
+
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+}
+
+impl Expression for IntegerLiteral {}
+
+impl Display for IntegerLiteral {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.value)
+    }
+}
+
+pub struct PrefixExpression {
+    pub token: Token,
+    pub operator: String,
+    pub right: Box<dyn Expression>,
+}
+
+impl Node for PrefixExpression {
+    fn token_literal(&self) -> &str {
+        return self.token.literal.as_str();
+    }
+
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+}
+
+impl Expression for PrefixExpression {}
+
+impl Display for PrefixExpression {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "({}{})", self.operator, self.right)
+    }
+}
+
+pub struct InfixExpression {
+    pub token: Token,
+    pub left: Box<dyn Expression>,
+    pub operator: String,
+    pub right: Box<dyn Expression>,
+}
+
+impl Node for InfixExpression {
+    fn token_literal(&self) -> &str {
+        return self.token.literal.as_str();
+    }
+
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+}
+
+impl Expression for InfixExpression {}
+
+impl Display for InfixExpression {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "({} {} {})", self.left, self.operator, self.right)
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::token::TokenType;
+
+    #[test]
+    fn string() {
+        let program = Program{
+            statements: vec![
+                Box::new(LetStatement{
+                    token: Token::new(TokenType::Let, "let"),
+                    name: Identifier{
+                        token: Token::new(TokenType::Ident, "myVar"),
+                        value: "myVar".to_string(),
+                    },
+                    value: Box::new(Identifier{
+                        token: Token::new(TokenType::Ident, "anotherVar"),
+                        value: "anotherVar".to_string(),
+                    })
+                })
+            ]
+        };
+
+        assert_eq!(format!("{}", program), "let myVar = anotherVar;");
+    }
 }
